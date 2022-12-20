bf057bd959a022c8050286739266b0d078c8f0fc
Lidia Makarchuk <lidiamakarcuk9@gmail.com>
2022-12-20 15:52:07 +0200

    Add graph class

---
+++ laba5/Graph.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba5
+{
+    public class Graph
+    {
+        private int _V;
+        private List<int> [] _adjustmentList;
+
+        public Graph(int v, int minVertexDegree, int maxVertexDegree)
+        {
+            this._V = v;
+            _adjustmentList = Generate(v, minVertexDegree, maxVertexDegree);
+        }
+        public static List<int>[] Generate(int v, int minVertexDegree, int maxVertexDegree)
+        {
+            List<int>[] adjList = new List<int>[v];
+
+            return adjList;
+        }
+    }
+}
