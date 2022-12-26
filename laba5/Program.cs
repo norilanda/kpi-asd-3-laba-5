e8ddba015300803ac9bf413d683f7fa9e73b3774
Lidia Makarchuk <lidiamakarcuk9@gmail.com>
2022-12-26 11:43:33 +0200

    Test algo

--- laba5/Program.cs
+++ laba5/Program.cs
@@ -2,71 +2,78 @@
 using laba5.GraphModel;
 using laba5.Testing;
 
-Test.CreateGraphs(10, 300, 2, 30);
+//Test.CreateGraphs(10, 300, 2, 30);
 
-Test.TestOnDiffGraphs(10);
+//Test.TestOnDiffGraphs(10);
 
-//string path = "graph.txt";
-//Graph graph1 = new Graph(300, 2, 30);
-//graph1.WriteToFile(path);
+string path = "graph.txt";
+int vertexNumber = 300;
+int minDegree = 2;
+int maxDegree = 30;
+Graph graph1 = new Graph(vertexNumber, minDegree, maxDegree);
+graph1.WriteToFile(path);
 
-//string pathA = "graphB.txt";
-//Graph graph = new Graph(path);
-//if (graph.V < 30)
-//{
-//    Console.WriteLine("Initial graph:\n");
-//    graph.Display();
-//}
-//Console.WriteLine();
+string pathA = "graphB.txt";
+Graph graph = new Graph(path);
 
-//int crossMethod = 2; //0-TwoPoints, 1-FivePoints, 2-Dynamic
-//int mutMethod = 1; //0-ChangeToOpposite, 1-Exchange
-//int imprMethod = 1; //0-AddVerticesToCliqueStraight, 1-AddVerticesToCliqueRandom, 2-AddVerticesFromAdjList
-//int terminationCondition = 1; //0-number of iterations, 1-stagnancy, 2-fullGraph
-//int terminationNumber = 1000;
-//double mutationPossibility = 0.1;
-//int k = 5;
+Console.WriteLine("Graph parameters: vertex number = " + vertexNumber + "; minDegree = " + minDegree + "; maxDegree = " + maxDegree + "\n");
+if (graph.V < 30)
+{
+    Console.WriteLine("Initial graph:\n");
+    graph.Display();
+}
+Console.WriteLine();
 
-//Console.Write("GA parametrs:\ncrossover method - ");
-//switch (crossMethod)
-//{
-//    case 0: Console.Write("TwoPoints; "); break;
-//    case 1: Console.Write("FivePoints; "); break;
-//    case 2: Console.Write("Dynamic; "); break;
-//    default: break;
-//}
-//Console.Write("mutation method - ");
-//switch (mutMethod)
-//{
-//    case 0: Console.Write("ChangeToOpposite; "); break;
-//    case 1: Console.Write("Exchange; "); break;
-//    default: break;
-//}
-//Console.Write("improvement method - ");
-//switch (imprMethod)
-//{
-//    case 1: Console.Write("AddVerticesToCliqueRandom; "); break;
-//    case 2: Console.Write("AddVerticesFromAdjList; "); break;
-//    default: break;
-//}
-//Console.Write("\ntermination condition - ");
-//switch (terminationCondition)
-//{
-//    case 0: Console.Write("number of iterations; "); break;
-//    case 1: Console.Write("stagnancy; "); break;
-//    default: break;
-//}
-//Console.Write("termination number - " + terminationNumber);
-//Console.WriteLine("\n");
+int crossMethod = 2; //0-TwoPoints, 1-FivePoints, 2-Dynamic
+int mutMethod = 0; //0-ChangeToOpposite, 1-Exchange
+int imprMethod = 2; //0-AddVerticesToCliqueStraight, 1-AddVerticesToCliqueRandom, 2-AddVerticesFromAdjList
+int terminationCondition = 1; //0-number of iterations, 1-stagnancy, 2-fullGraph
+int terminationNumber = 200;
+double mutationPossibility = 0.1;
+int k = 4;
+Console.Write("Enter clique size to search for: ");
+k = Convert.ToInt32(Console.ReadLine());
 
-//GeneticAlgorithm ga = new GeneticAlgorithm(graph, crossMethod, mutMethod, mutationPossibility, imprMethod, terminationCondition, terminationNumber);
-//ga.Start(k);
-//Creature bestCreature = ga.BestCreature;
-//if (ga.HasAClique)
-//    Console.WriteLine("Graph CONTAINS clique with " + k + " vertices");
-//else
-//    Console.WriteLine("The maximum clique has less than " + k + " vertices. Graph DOESN'T contain clique with " + k + " vertices");
-//Console.WriteLine("Maximum clique: ");
-//bestCreature.DisplayMaxClique();
+Console.Write("GA parametrs:\ncrossover method - ");
+switch (crossMethod)
+{
+    case 0: Console.Write("TwoPoints; "); break;
+    case 1: Console.Write("FivePoints; "); break;
+    case 2: Console.Write("Dynamic; "); break;
+    default: break;
+}
+Console.Write("mutation method - ");
+switch (mutMethod)
+{
+    case 0: Console.Write("ChangeToOpposite; "); break;
+    case 1: Console.Write("Exchange; "); break;
+    default: break;
+}
+Console.Write("improvement method - ");
+switch (imprMethod)
+{
+    case 1: Console.Write("AddVerticesToCliqueRandom; "); break;
+    case 2: Console.Write("AddVerticesFromAdjList; "); break;
+    default: break;
+}
+Console.Write("\ntermination condition - ");
+switch (terminationCondition)
+{
+    case 0: Console.Write("number of iterations; "); break;
+    case 1: Console.Write("stagnancy; "); break;
+    default: break;
+}
+Console.Write("termination number - " + terminationNumber);
+Console.WriteLine("\n");
 
-//Console.WriteLine("Number of iterations that has been made: " + ga.Iterations);
+GeneticAlgorithm ga = new GeneticAlgorithm(graph, crossMethod, mutMethod, mutationPossibility, imprMethod, terminationCondition, terminationNumber);
+ga.Start(k);
+Creature bestCreature = ga.BestCreature;
+if (ga.HasAClique)
+    Console.WriteLine("Graph CONTAINS clique with " + k + " vertices");
+else
+    Console.WriteLine("The maximum clique has less than " + k + " vertices. Graph DOESN'T contain clique with " + k + " vertices");
+Console.WriteLine("Maximum clique: ");
+bestCreature.DisplayMaxClique();
+
+Console.WriteLine("Number of iterations that has been made: " + ga.Iterations);
--- laba5/Testing/Test.cs
+++ laba5/Testing/Test.cs
@@ -22,13 +22,14 @@ namespace laba5.Testing
         }
         public static void TestOnDiffGraphs(int GNumber)
         {
-            int crossMethod = 0; //0-TwoPoints, 1-FivePoints, 2-Dynamic
+            int crossMethod = 2; //0-TwoPoints, 1-FivePoints, 2-Dynamic
             int mutMethod = 1; //0-ChangeToOpposite, 1-Exchange
-            int imprMethod = 1; //0-AddVerticesToCliqueStraight, 1-AddVerticesToCliqueRandom, 2-AddVerticesFromAdjList
+            int imprMethod = 2; //0-AddVerticesToCliqueStraight, 1-AddVerticesToCliqueRandom, 2-AddVerticesFromAdjList
             int terminationCondition = 1; //0-number of iterations, 1-stagnancy, 2-fullGraph
             int terminationNumber = 300;
             double mutationPossibility = 0.1;
             int k = 5;
+            string[] conclusion = new string[GNumber];
             for (int i = 0; i < GNumber; i++)
             {
                 Console.WriteLine("Number " + (int)(i + 1));
@@ -42,16 +43,26 @@ namespace laba5.Testing
                 GeneticAlgorithm ga = new GeneticAlgorithm(graph, crossMethod, mutMethod, mutationPossibility, imprMethod, terminationCondition, terminationNumber);
                 ga.Start(k);
                 Creature bestCreature = ga.BestCreature;
+                conclusion[i] += ga.Iterations;
                 if (ga.HasAClique)
+                {
                     Console.WriteLine("Graph CONTAINS clique with " + k + " vertices");
+                    conclusion[i] += " +";
+                }
                 else
+                { 
                     Console.WriteLine("The maximum clique has less than " + k + " vertices. Graph DOESN'T contain clique with " + k + " vertices");
+                    conclusion[i] += " -";
+                }
                 Console.WriteLine("Maximum clique: ");
                 bestCreature.DisplayMaxClique();
 
                 Console.WriteLine("Number of iterations that has been made: " + ga.Iterations);
-                Console.WriteLine();
+                Console.WriteLine();                
             }
+            Console.WriteLine();
+            for (int i = 0; i < GNumber; i++)
+                Console.WriteLine(conclusion[i]);
         }
     }
 }
